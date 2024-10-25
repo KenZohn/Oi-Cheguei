052e320dfb41f9f71cfe1dadf53018dabea6e94d
Johnny <125989975+KenZohn@users.noreply.github.com>
2024-10-24 23:41:17 -0300

    Mouse Enter e Leave dos botões implementado

--- CadastroResponsavelAluno/MainWindow.xaml.cs
+++ CadastroResponsavelAluno/MainWindow.xaml.cs
@@ -23,15 +23,123 @@ namespace CadastroResponsavelAluno
     public partial class MainWindow : Window
     {
         PageGestor pageGestor;
+        PageProfessor pageProfessor;
+        PagePorteiro pagePorteiro;
         public MainWindow()
         {
             InitializeComponent();
+
             pageGestor = new PageGestor();
+            pageProfessor = new PageProfessor();
+            pagePorteiro = new PagePorteiro();
+
+            BotaoGestor_MouseLeftButtonUp(BotaoGestor, null);
         }
 
         private void BotaoGestor_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            AlterarCorBotaoGestor();
             FramePrincipal.Navigate(pageGestor);
+        }
+
+        private void BotaoProfessor_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            AlterarCorBotaoProfessor();
+            FramePrincipal.Navigate(pageProfessor);
+        }
+
+        private void BotaoPorteiro_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            AlterarCorBotaoPorteiro();
+            FramePrincipal.Navigate(pagePorteiro);
+        }
+
+        private void BotaoSair_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AlterarCorBotaoGestor()
+        {
+            ResetarCorBotoes();
+            BotaoGestor.Background = (Brush)new BrushConverter().ConvertFromString("#E6E6E6");
+        }
+
+        private void AlterarCorBotaoProfessor()
+        {
+            ResetarCorBotoes();
+            BotaoProfessor.Background = (Brush)new BrushConverter().ConvertFromString("#E6E6E6");
+        }
+
+        private void AlterarCorBotaoPorteiro()
+        {
+            ResetarCorBotoes();
+            BotaoPorteiro.Background = (Brush)new BrushConverter().ConvertFromString("#E6E6E6");
+        }
+
+        private void ResetarCorBotoes()
+        {
+            BotaoGestor.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
+            BotaoProfessor.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
+            BotaoPorteiro.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
+        }
+
+        private void BotaoGestor_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (BotaoGestor.Background.ToString() != "#FFE6E6E6")
+            {
+                BotaoGestor.Background = (Brush)new BrushConverter().ConvertFromString("#F2F2F2");
+            }
+        }
+
+        private void BotaoGestor_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (BotaoGestor.Background.ToString() != "#FFE6E6E6")
+            {
+                BotaoGestor.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
+            }
+        }
+
+        private void BotaoProfessor_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (BotaoProfessor.Background.ToString() != "#FFE6E6E6")
+            {
+                BotaoProfessor.Background = (Brush)new BrushConverter().ConvertFromString("#F2F2F2");
+            }
+        }
+
+        private void BotaoProfessor_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (BotaoProfessor.Background.ToString() != "#FFE6E6E6")
+            {
+                BotaoProfessor.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
+            }
+        }
+
+        private void BotaoPorteiro_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (BotaoPorteiro.Background.ToString() != "#FFE6E6E6")
+            {
+                BotaoPorteiro.Background = (Brush)new BrushConverter().ConvertFromString("#F2F2F2");
+            }
+        }
+
+        private void BotaoPorteiro_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (BotaoPorteiro.Background.ToString() != "#FFE6E6E6")
+            {
+                BotaoPorteiro.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
+            }
+        }
+
+        private void BotaoSair_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BotaoSair.Background = (Brush)new BrushConverter().ConvertFromString("#F2F2F2");
+        }
+
+        private void BotaoSair_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BotaoSair.Background = (Brush)new BrushConverter().ConvertFromString("#FFFFFF");
         }
     }
 }
--- CadastroResponsavelAluno/PageGestor.xaml.cs
+++ CadastroResponsavelAluno/PageGestor.xaml.cs
@@ -22,27 +22,34 @@ namespace CadastroResponsavelAluno
     //Alterar caminho da imagem quando mudar para o projeto principal
     public partial class PageGestor : Page
     {
+        PageListaAlunos pageListaAlunos;
+        PageCadastroAluno pageCadastroAluno;
         public PageGestor()
         {
             InitializeComponent();
+
+            pageListaAlunos = new PageListaAlunos();
+            pageCadastroAluno = new PageCadastroAluno();
+
             BotaoListarAlunos_MouseLeftButtonUp(BotaoListarAlunos, null);
         }
 
         private void BotaoListarAlunos_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            ResetarCorBotoes();
             AlterarCorBotaoListarAlunos();
+            FrameSecundario.Navigate(pageListaAlunos);
         }
 
         private void BotaoCadastrarAluno_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            ResetarCorBotoes();
             AlterarCorBotaoCadastrarAluno();
-            FrameSecundario.Navigate(new PageCadastroAluno());
+            FrameSecundario.Navigate(pageCadastroAluno);
         }
 
         private void AlterarCorBotaoListarAlunos()
         {
+            ResetarCorBotoes();
+            BotaoListarAlunos.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#0F0F0F");
             BotaoListarAlunos.BorderThickness = new Thickness(0, 0, 0, 3);
             ImageListarAlunos.Source = new BitmapImage(new Uri("pack://application:,,,/CadastroResponsavelAluno;component/Icons/Listar.png")); //Alterar caminho da imagem quando mudar para o projeto principal
             LabelListarAlunos.Foreground = (Brush)new BrushConverter().ConvertFromString("#0F0F0F");
@@ -51,6 +58,8 @@ namespace CadastroResponsavelAluno
 
         private void AlterarCorBotaoCadastrarAluno()
         {
+            ResetarCorBotoes();
+            BotaoCadastrarAluno.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#0F0F0F");
             BotaoCadastrarAluno.BorderThickness = new Thickness(0, 0, 0, 3);
             ImageCadastrarAluno.Source = new BitmapImage(new Uri("pack://application:,,,/CadastroResponsavelAluno;component/Icons/Adicionar.png")); //Alterar caminho da imagem quando mudar para o projeto principal
             LabelCadastrarAluno.Foreground = (Brush)new BrushConverter().ConvertFromString("#0F0F0F");
@@ -59,15 +68,49 @@ namespace CadastroResponsavelAluno
 
         private void ResetarCorBotoes()
         {
+            BotaoCadastrarAluno.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#606060");
             BotaoCadastrarAluno.BorderThickness = new Thickness(0, 0, 0, 0);
             ImageCadastrarAluno.Source = new BitmapImage(new Uri("pack://application:,,,/CadastroResponsavelAluno;component/Icons/AdicionarApagado.png")); //Alterar caminho da imagem quando mudar para o projeto principal
             LabelCadastrarAluno.Foreground = (Brush)new BrushConverter().ConvertFromString("#606060");
             LabelCadastrarAluno.FontWeight = FontWeights.Regular;
 
+            BotaoListarAlunos.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#606060");
             BotaoListarAlunos.BorderThickness = new Thickness(0, 0, 0, 0);
             ImageListarAlunos.Source = new BitmapImage(new Uri("pack://application:,,,/CadastroResponsavelAluno;component/Icons/ListarApagado.png")); //Alterar caminho da imagem quando mudar para o projeto principal
             LabelListarAlunos.Foreground = (Brush)new BrushConverter().ConvertFromString("#606060");
             LabelListarAlunos.FontWeight = FontWeights.Regular;
+        }
+
+        private void BotaoListarAlunos_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(BotaoListarAlunos.BorderBrush.ToString() != "#FF0F0F0F")
+            {
+                BotaoListarAlunos.BorderThickness = new Thickness(0, 0, 0, 3);
+            }
+        }
+
+        private void BotaoListarAlunos_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (BotaoListarAlunos.BorderBrush.ToString() != "#FF0F0F0F")
+            {
+                BotaoListarAlunos.BorderThickness = new Thickness(0, 0, 0, 0);
+            }
+        }
+
+        private void BotaoCadastrarAluno_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (BotaoCadastrarAluno.BorderBrush.ToString() != "#FF0F0F0F")
+            {
+                BotaoCadastrarAluno.BorderThickness = new Thickness(0, 0, 0, 3);
+            }
+        }
+
+        private void BotaoCadastrarAluno_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (BotaoCadastrarAluno.BorderBrush.ToString() != "#FF0F0F0F")
+            {
+                BotaoCadastrarAluno.BorderThickness = new Thickness(0, 0, 0, 0);
+            }
         }
     }
 }
