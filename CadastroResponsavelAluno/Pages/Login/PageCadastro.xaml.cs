a3d0e17be33556df4f7a0bd8abc8a2523d035d75
João Crespin <joaorato58@gmail.com>
2024-11-25 16:26:46 -0300

    CPF na página de cadastro de funcionário formata automáticamente.

--- CadastroResponsavelAluno/Pages/Login/PageCadastro.xaml.cs
+++ CadastroResponsavelAluno/Pages/Login/PageCadastro.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,6 +33,7 @@ namespace CadastroResponsavelAluno
         {
             try
             {
+                string cpf = CampoCPF.Text.Replace(".", "").Replace("-", "").Replace(" ", "");
                 if (string.IsNullOrWhiteSpace(CampoNome.Text) || string.IsNullOrWhiteSpace(CampoSenha.Password) || string.IsNullOrWhiteSpace(CampoCPF.Text) || string.IsNullOrWhiteSpace(ComboBoxCargo.Text))
                 {
                     MessageBox.Show("Preencha os campos vazios!");
@@ -40,14 +42,14 @@ namespace CadastroResponsavelAluno
                 {
                     MessageBox.Show("As senhas não coincidem.");
                 }
-                else if (CampoCPF.Text.Length != 11 || !CampoCPF.Text.All(char.IsDigit))
+                else if (cpf.Length != 11 || !cpf.All(char.IsDigit))
                 {
                     MessageBox.Show("CPF inválido!");
                 }
                 else
                 {
                     Usuario user = new Usuario(_conexao);
-                    user.MetodoCadastro(CampoNome.Text, CampoSenha.Password, CampoCPF.Text, ComboBoxCargo.Text);
+                    user.MetodoCadastro(CampoNome.Text, CampoSenha.Password, cpf, ComboBoxCargo.Text);
                     MessageBox.Show("Cadastrado com sucesso!");
 
                     CampoNome.Clear();
@@ -63,45 +65,33 @@ namespace CadastroResponsavelAluno
             }
         }
 
-        private void CampoSenha_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            //if (string.IsNullOrEmpty(CampoSenha.Password))
-            //{
-            //    LabelSenha.Visibility = Visibility.Visible;
-            //}
-            //else
-            //{
-            //    LabelSenha.Visibility = Visibility.Collapsed;
-            //}
-        }
-
-        private void CampoRepetirSenha_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            //if (string.IsNullOrEmpty(CampoRepetirSenha.Password))
-            //{
-            //    LabelRepetirSenha.Visibility = Visibility.Visible;
-            //}
-            //else
-            //{
-            //    LabelRepetirSenha.Visibility = Visibility.Collapsed;
-            //}
-        }
-
-        private void ComboBoxCargo_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //if (ComboBoxCargo.Text != null)
-            //{
-            //    labelCargo.Visibility = Visibility.Collapsed;
-            //}
-            //else
-            //{
-            //    labelCargo.Visibility = Visibility.Visible;
-            //}
-        }
 
         private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.NavigationService.Navigate(new PageLogin());
         }
+
+        private void CampoCPF_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            // Remove caracteres inválidos
+            string text = Regex.Replace(textBox.Text, @"[^\d]", "");
+            // Formatando com caracteres especiais
+            if (text.Length > 0)
+            {
+                if (text.Length > 3 && text.Length <= 6)
+                    text = $"{text.Substring(0, 3)}.{text.Substring(3, text.Length - 3)}";
+                else if (text.Length > 6 && text.Length <= 9)
+                    text = $"{text.Substring(0, 3)}.{text.Substring(3, 3)}.{text.Substring(6, text.Length - 6)}";
+                else if (text.Length > 9)
+                    text = $"{text.Substring(0, 3)}.{text.Substring(3, 3)}.{text.Substring(6, 3)}-{text.Substring(9, text.Length - 9)}";
+            }
+            textBox.TextChanged -= CampoCPF_TextChanged;
+            textBox.Text = text;
+            textBox.CaretIndex = textBox.Text.Length;
+            textBox.TextChanged += CampoCPF_TextChanged;
+        }
+
+
     }
 }
