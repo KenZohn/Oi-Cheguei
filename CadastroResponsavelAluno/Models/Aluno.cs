631db9cc65f2ebd3a01c54a0e657b924c4c4255e
Johnny <125989975+KenZohn@users.noreply.github.com>
2024-11-25 22:49:22 -0300

    Ajuste

---
+++ CadastroResponsavelAluno/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroResponsavelAluno
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+        public string Responsavel { get; set; }
+        public string CPFAluno { get; set; }
+        public string CPFResponsavel { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string TelefoneTrabalho { get; set; }
+        public string EnderecoTrabalho { get; set; }
+        public string Autorizado1 { get; set; }
+        public string Autorizado2 { get; set; }
+        public string Autorizado3 { get; set; }
+        public bool Presente { get; set; }
+    }
+}
---
+++ CadastroResponsavelAluno/Models/ConexaoBD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroResponsavelAluno.Models
+{
+    public class ConexaoBD
+    {
+        private SQLiteConnection conn;
+        private string stringConexao = "Data Source=" + System.IO.Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName) + "/ChegouBD.db";
+
+        public ConexaoBD()
+        {
+            conn = new SQLiteConnection(stringConexao);
+        }
+
+        public SQLiteConnection AbrirConexao()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        public void FecharConexao()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
